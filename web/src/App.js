091f04e37f9d466649a61d5ba00fdf55a70c46a2
Nick LoVallo <nicholas.lovallo@gmail.com>
2021-05-28 13:18:51 -0500

    minor changes to website and update README

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import firebase from "firebase";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
